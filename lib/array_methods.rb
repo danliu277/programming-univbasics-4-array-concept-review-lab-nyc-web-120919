8abbe11745f4a55b416e35262f101ce453bb673d
Daniel Liu <danliu277@gmail.com>
2019-12-02 18:45:44 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,7 +4,8 @@ def find_element_index(array, value_to_find)
   counter = 0;
   while array[counter] do
     if(array[counter] == value_to_find)
-      index = counter;
+      index = counter
+    counter += 1
   end
   return counter
 end
@@ -15,7 +16,8 @@ def find_max_value(array)
   counter = 0;
   while array[counter] do
     if(array[counter] > max)
-      max = array[counter];
+      max = array[counter]
+    counter += 1
   end
   return max
 end
@@ -26,7 +28,8 @@ def find_min_value(array)
   counter = 0;
   while array[counter] do
     if(array[counter] < min)
-      min = array[counter];
+      min = array[counter]
+    counter += 1
   end
   return min
 end
