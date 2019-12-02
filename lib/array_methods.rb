6277926ba4a39205f07857ecc01a456f10b04c44
Daniel Liu <danliu277@gmail.com>
2019-12-02 18:45:10 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -14,7 +14,7 @@ def find_max_value(array)
   max = 0
   counter = 0;
   while array[counter] do
-    if(array[counter] >= max)
+    if(array[counter] > max)
       max = array[counter];
   end
   return max
@@ -22,4 +22,11 @@ end
 
 def find_min_value(array)
   # Add your solution here
+  min = 0
+  counter = 0;
+  while array[counter] do
+    if(array[counter] < min)
+      min = array[counter];
+  end
+  return min
 end
